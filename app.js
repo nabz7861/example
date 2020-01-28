20f08de54a173c94a4dfdd1248a395e6a249bf20
Nabeel Hussain <nab_h@hotmail.co.uk>
2020-01-28 18:35:34 +0000

    dasd

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 // Generating content based on the template
 var template = "<article>\n\
 	<img src='data/img/placeholder.png' data-src='data/img/SLUG.jpg' alt='NAME'>\n\
-	
+	<h3>#POS. NAME</h3>\n\
 	<ul>\n\
-
+	<li><span>Author:</span> <strong>AUTHOR</strong></li>\n\
+	<li><span>Twitter:</span> <a href='https://twitter.com/TWITTER'>@TWITTER</a></li>\n\
+	<li><span>Website:</span> <a href='http://WEBSITE/'>WEBSITE</a></li>\n\
+	<li><span>GitHub:</span> <a href='https://GITHUB'>GITHUB</a></li>\n\
+	<li><span>More:</span> <a href='http://js13kgames.com/entries/SLUG'>js13kgames.com/entries/SLUG</a></li>\n\
 	</ul>\n\
 </article>";
 var content = '';
@@ -11,7 +15,10 @@ for(var i=0; i<games.length; i++) {
 	var entry = template.replace(/POS/g,(i+1))
 		.replace(/SLUG/g,games[i].slug)
 		.replace(/NAME/g,games[i].name)
-
+		.replace(/AUTHOR/g,games[i].author)
+		.replace(/TWITTER/g,games[i].twitter)
+		.replace(/WEBSITE/g,games[i].website)
+		.replace(/GITHUB/g,games[i].github);
 	entry = entry.replace('<a href=\'http:///\'></a>','-');
 	content += entry;
 };
