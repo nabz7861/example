66014ccb7afd433dcd37b241a7b50068554699eb
Nabeel Hussain <nab_h@hotmail.co.uk>
2020-01-28 14:53:43 +0000

    first

--- data/games.js
+++ data/games.js
@@ -1,24 +1,175 @@
 var games = [
-	
 	{
-		slug: 'course10',
-		name: 'course10',
+		slug: 'lost-in-cyberspace',
+		name: 'Lost in Cyberspace',
+		author: 'Zosia and Bartek',
+		twitter: 'bartaz',
+		website: '',
+		github: 'github.com/bartaz/lost-in-cyberspace'
+	},
+	{
+		slug: 'vernissage',
+		name: 'Vernissage',
+		author: 'Platane',
+		twitter: 'platane_',
+		website: 'github.com/Platane',
+		github: 'github.com/Platane/js13k-2017'
+	},
+	{
+		slug: 'coconutty',
+		name: 'Coconutty',
+		author: 'Mary Knize',
+		twitter: 'captainpainway',
+		website: 'maryknize.com',
+		github: 'github.com/captainpainway/coconutty'
+	},
+	{
+		slug: 'lost-pacman',
+		name: 'Lost Pacman',
+		author: 'MarcGuinea',
+		twitter: 'MarcGuineaCasas',
+		website: 'marcguinea.com',
+		github: 'github.com/mguinea/lost-pacman'
+	},
+	{
+		slug: 'polyhedron-runner',
+		name: 'Polyhedron Runner',
+		author: 'Alex Swan',
+		twitter: 'BoldBigflank',
+		website: 'bold-it.com',
+		github: 'github.com/BoldBigflank/js13k-polyhedron'
+	},
+	{
+		slug: 'she-is-my-universe',
+		name: 'She is my universe',
+		author: 'Madmarcel',
+		twitter: 'madmarcel',
+		website: '',
+		github: 'github.com/madmarcel/js13k2017'
+	},
+	{
+		slug: 'spacewrecked',
+		name: 'Spacewrecked',
+		author: 'Sorskoot',
+		twitter: 'Sorskoot',
+		website: 'timmykokke.com',
+		github: 'github.com/sorskoot/js13kgames_2017_Lost'
+	},
+	{
+		slug: 'shifted-dimensions',
+		name: 'Shifted Dimensions',
+		author: 'Nylki',
+		twitter: 'nylk',
+		website: 'github.com/nylki',
+		github: 'github.com/nylki/shifted-dimensions'
+	},
+	{
+		slug: 'wandering-moon',
+		name: 'Wandering Moon',
+		author: 'Jack Greenberg',
+		twitter: 'thprgrmmrjck',
+		website: '',
+		github: 'github.com/theProgrammerJack/js13k2017'
+	},
+	{
+		slug: 'lost-in-guam',
+		name: 'Lost in Guam',
+		author: 'Kenneth Banico',
+		twitter: 'kjdesigns671',
+		website: '',
+		github: 'github.com/kbanico/lost-in-guam-vr-game'
+	},
+	{
+		slug: 'balloon-problems',
+		name: 'Balloon Problems',
+		author: 'Fasility',
+		twitter: 'Fasility_VR',
+		website: 'fasility.com',
+		github: 'github.com/flowerio/balloon-problems'
+	},
+	{
+		slug: 'lost-in-my-mind',
+		name: 'Lost in my mind',
+		author: 'Lasagne Games',
+		twitter: 'Lazyeels',
+		website: '',
+		github: 'github.com/lazyeels/js13kb'
+	},
+	{
+		slug: 'lost-in-the-forest-dungeon',
+		name: 'Lost In The Forest Dungeon',
+		author: 'Luke',
+		twitter: 'cannl',
+		website: 'lc-apps.co.uk',
+		github: 'github.com/lcapps-luke/js13k-lost'
+	},
+	{
+		slug: 'galacticdiamond',
+		name: 'GalacticDiamond',
+		author: 'Mitruska',
+		twitter: 'mitruska_',
+		website: '',
+		github: 'github.com/mitruch/GalacticDiamond-JS13KGames-2017'
+	},
+	{
+		slug: 'cat-meow',
+		name: 'Cat Meow',
+		author: 'Lislis',
+		twitter: '',
+		website: '',
+		github: 'github.com/lislis/cat-meow'
+	},
+	{
+		slug: 'metamorphosis',
+		name: 'Metamorphosis',
+		author: 'Steff and Tanyuan',
+		twitter: '',
+		website: '',
+		github: 'github.com/tanyuan/metamorphosis'
+	},
+	{
+		slug: 'a-snake',
+		name: 'A-Snake',
+		author: 'Nick Frazier',
+		twitter: 'nrf',
+		website: 'nickfrazier.com',
+		github: 'github.com/fraziern/vrsnake'
+	},
+	{
+		slug: 'wherewhat',
+		name: 'Where? What?',
+		author: '..Katu..',
+		twitter: '',
+		website: '',
+		github: 'github.com/katubrd/LostVR'
+	},
+	{
+		slug: 'dont-let-your-dreams-be-memes',
+		name: 'Don\'t let your dreams be memes',
+		author: 'Mark Vasilkov',
+		twitter: 'mvasilkov',
+		website: 'mvasilkov.ovh',
+		github: 'github.com/mvasilkov/aframe13k'
+	},
+	{
+		slug: 'fly-south',
+		name: 'Fly South',
 		author: 'Christian Paul (jaller94)',
 		twitter: '',
 		website: 'chrpaul.de',
 		github: 'github.com/jaller94/fly-south'
 	},
 	{
-		slug: 'course9',
-		name: 'course9',
+		slug: 'prisonri0t',
+		name: 'PrisonRi0t',
 		author: 'Sondor',
 		twitter: '',
 		website: '',
 		github: 'github.com/gabboraron/prison'
 	},
 	{
-		slug: 'course8',
-		name: 'course8',
+		slug: 'course4',
+		name: 'DebrISS',
 		author: 'Kovolmany',
 		twitter: '',
 		website: '',
