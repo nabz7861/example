6a80345462be823f3cf4bc6b0f68442be34e01ec
Nabeel Hussain <nab_h@hotmail.co.uk>
2020-01-30 19:58:29 +0000

    final

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var template = "<article>\n\
 	<li><span>course-Price:</span> <strong> price</strong></li>\n\
 	<li><span>courseTopic:</span> topic</a></li>\n\
 	<li><span>review:</span> rating</a></li>\n\
-	<li><span>loki</span> locate</a></li>\n\
+	<li><span>location</span> locate</a></li>\n\
 	\n\
 	</ul>\n\
 </article>";
