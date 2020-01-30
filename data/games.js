71081a002e17b00338b3612aec966fda20a6a016
Nabeel Hussain <nab_h@hotmail.co.uk>
2020-01-30 18:38:42 +0000

    w

--- data/games.js
+++ data/games.js
@@ -2,82 +2,82 @@ var games = [
 	{
 		slug: 'course1',
 		name: 'course1',
-		price: '£3',
-		topic: 'maths',
-		location: 'hendon',
-		review: 'github.com/bartaz/lost-in-cyberspace'
+		price: 'Zosia and Bartek',
+		twitter: 'bartaz',
+		website: '',
+		github: 'github.com/bartaz/lost-in-cyberspace'
 	},
 	{
 		slug: 'course2',
 		name: 'course2',
-		price: '£3',
-		topic: 'maths',
-		location: 'hendon',
+		price: 'Platane',
+		twitter: 'platane_',
+		website: 'github.com/Platane',
 		github: 'github.com/Platane/js13k-2017'
 	},
 	{
 		slug: 'course3',
 		name: 'course3',
-		price: '£3',
-		topic: 'maths',
-		location: 'hendon',
+		price: 'Mary Knize',
+		twitter: 'captainpainway',
+		website: 'maryknize.com',
 		github: 'github.com/captainpainway/coconutty'
 	},
 	{
 		slug: 'course4',
 		name: 'course4',
-		price: '£3',
-		topic: 'maths',
-		location: 'hendon',
+		price: 'MarcGuinea',
+		twitter: 'MarcGuineaCasas',
+		website: 'marcguinea.com',
 		github: 'github.com/mguinea/lost-pacman'
 	},
 	{
 		slug: 'course5',
 		name: 'course5',
-		price: '£3',
-		topic: 'maths',
-		location: 'hendon',
+		price: 'Alex Swan',
+		twitter: 'BoldBigflank',
+		website: 'bold-it.com',
 		github: 'github.com/BoldBigflank/js13k-polyhedron'
 	},
 	{
 		slug: 'course6',
 		name: 'course6',
-		price: '£3',
-		topic: 'maths',
-		location: 'hendon',
+		price: 'Madmarcel',
+		twitter: 'madmarcel',
+		website: '',
 		github: 'github.com/madmarcel/js13k2017'
 	},
 	{
 		slug: 'course7',
 		name: 'course7',
-		price: '£3',
-		topic: 'maths',
-		location: 'hendon',
+		price: 'Sorskoot',
+		twitter: 'Sorskoot',
+		website: 'timmykokke.com',
 		github: 'github.com/sorskoot/js13kgames_2017_Lost'
 	},
 	{
 		slug: 'course8',
 		name: 'course8',
-		price: '£3',
-		topic: 'maths',
-		location: 'hendon',
+		price: 'Nylki',
+		twitter: 'nylk',
+		website: 'github.com/nylki',
 		github: 'github.com/nylki/shifted-dimensions'
 	},
 	{
 		slug: 'course9',
 		name: 'course9',
-		price: '£3',
-		topic: 'maths',
-		location: 'hendon',
+		price: 'Jack Greenberg',
+		twitter: 'thprgrmmrjck',
+		website: '',
 		github: 'github.com/theProgrammerJack/js13k2017'
 	},
 	
 	{
 		slug: 'course10',
 		name: 'course10',
-		price: '£3',
-		topic: 'maths',
-		location: 'hendon',
+		price: 'Karan Ganesan',
+		twitter: 'karanganesan',
+		website: 'linkedin.com/in/karanganesan',
 		github: 'github.com/karanganesan/Lost_in_Metaverse'
 	}
 ];
--- data/gamesm.js
+++ data/gamesm.js
@@ -2,82 +2,82 @@ var games = [
 	{
 		slug: 'course1',
 		name: 'course1',
-		author: 'Zosia and Bartek',
-		twitter: 'bartaz',
-		website: '',
-		github: 'github.com/bartaz/lost-in-cyberspace'
+		price: '£3',
+		topic: 'maths',
+		location: 'hendon',
+		review: 'github.com/bartaz/lost-in-cyberspace'
 	},
 	{
 		slug: 'course2',
 		name: 'course2',
-		author: 'Platane',
-		twitter: 'platane_',
-		website: 'github.com/Platane',
+		price: '£3',
+		topic: 'maths',
+		location: 'hendon',
 		github: 'github.com/Platane/js13k-2017'
 	},
 	{
 		slug: 'course3',
 		name: 'course3',
-		author: 'Mary Knize',
-		twitter: 'captainpainway',
-		website: 'maryknize.com',
+		price: '£3',
+		topic: 'maths',
+		location: 'hendon',
 		github: 'github.com/captainpainway/coconutty'
 	},
 	{
 		slug: 'course4',
 		name: 'course4',
-		author: 'MarcGuinea',
-		twitter: 'MarcGuineaCasas',
-		website: 'marcguinea.com',
+		price: '£3',
+		topic: 'maths',
+		location: 'hendon',
 		github: 'github.com/mguinea/lost-pacman'
 	},
 	{
 		slug: 'course5',
 		name: 'course5',
-		author: 'Alex Swan',
-		twitter: 'BoldBigflank',
-		website: 'bold-it.com',
+		price: '£3',
+		topic: 'maths',
+		location: 'hendon',
 		github: 'github.com/BoldBigflank/js13k-polyhedron'
 	},
 	{
 		slug: 'course6',
 		name: 'course6',
-		author: 'Madmarcel',
-		twitter: 'madmarcel',
-		website: '',
+		price: '£3',
+		topic: 'maths',
+		location: 'hendon',
 		github: 'github.com/madmarcel/js13k2017'
 	},
 	{
 		slug: 'course7',
 		name: 'course7',
-		author: 'Sorskoot',
-		twitter: 'Sorskoot',
-		website: 'timmykokke.com',
+		price: '£3',
+		topic: 'maths',
+		location: 'hendon',
 		github: 'github.com/sorskoot/js13kgames_2017_Lost'
 	},
 	{
 		slug: 'course8',
 		name: 'course8',
-		author: 'Nylki',
-		twitter: 'nylk',
-		website: 'github.com/nylki',
+		price: '£3',
+		topic: 'maths',
+		location: 'hendon',
 		github: 'github.com/nylki/shifted-dimensions'
 	},
 	{
 		slug: 'course9',
 		name: 'course9',
-		author: 'Jack Greenberg',
-		twitter: 'thprgrmmrjck',
-		website: '',
+		price: '£3',
+		topic: 'maths',
+		location: 'hendon',
 		github: 'github.com/theProgrammerJack/js13k2017'
 	},
 	
 	{
 		slug: 'course10',
 		name: 'course10',
-		author: 'Karan Ganesan',
-		twitter: 'karanganesan',
-		website: 'linkedin.com/in/karanganesan',
+		price: '£3',
+		topic: 'maths',
+		location: 'hendon',
 		github: 'github.com/karanganesan/Lost_in_Metaverse'
 	}
 ];
