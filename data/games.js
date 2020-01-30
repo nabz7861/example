3d6206b0ebbb042b40241bc3524479cae3436610
Nabeel Hussain <nab_h@hotmail.co.uk>
2020-01-30 18:58:44 +0000

    w

--- data/games.js
+++ data/games.js
@@ -3,71 +3,71 @@ var games = [
 		slug: 'course1',
 		name: 'course1',
 		price: 'Zosia and Bartek',
-		topic: 'bartaz',
+		topic: 'math',
 		website: '',
 		github: 'github.com/bartaz/lost-in-cyberspace'
 	},
 	{
 		slug: 'course2',
 		name: 'course2',
-		price: 'Platane',
-		twitter: 'platane_',
+		price: '£3',
+		topic: 'math',
 		website: 'github.com/Platane',
 		github: 'github.com/Platane/js13k-2017'
 	},
 	{
 		slug: 'course3',
 		name: 'course3',
-		price: 'Mary Knize',
-		twitter: 'captainpainway',
+		price: '£3',
+		topic: 'math',
 		website: 'maryknize.com',
 		github: 'github.com/captainpainway/coconutty'
 	},
 	{
 		slug: 'course4',
 		name: 'course4',
-		price: 'MarcGuinea',
-		twitter: 'MarcGuineaCasas',
+		price: '£3',
+		topic: 'math',
 		website: 'marcguinea.com',
 		github: 'github.com/mguinea/lost-pacman'
 	},
 	{
 		slug: 'course5',
 		name: 'course5',
-		price: 'Alex Swan',
-		twitter: 'BoldBigflank',
+		price: '£3',
+		topic: 'math',
 		website: 'bold-it.com',
 		github: 'github.com/BoldBigflank/js13k-polyhedron'
 	},
 	{
 		slug: 'course6',
 		name: 'course6',
-		price: 'Madmarcel',
-		twitter: 'madmarcel',
+		price: '£3',
+		topic: 'math',
 		website: '',
 		github: 'github.com/madmarcel/js13k2017'
 	},
 	{
 		slug: 'course7',
 		name: 'course7',
-		price: 'Sorskoot',
-		twitter: 'Sorskoot',
+		price: '£3',
+		topic: 'math',
 		website: 'timmykokke.com',
 		github: 'github.com/sorskoot/js13kgames_2017_Lost'
 	},
 	{
 		slug: 'course8',
 		name: 'course8',
-		price: 'Nylki',
-		twitter: 'nylk',
+		price: '£3',
+		topic: 'math',
 		website: 'github.com/nylki',
 		github: 'github.com/nylki/shifted-dimensions'
 	},
 	{
 		slug: 'course9',
 		name: 'course9',
-		price: 'Jack Greenberg',
-		twitter: 'thprgrmmrjck',
+		price: '£3',
+		topic: 'math',
 		website: '',
 		github: 'github.com/theProgrammerJack/js13k2017'
 	},
@@ -75,8 +75,8 @@ var games = [
 	{
 		slug: 'course10',
 		name: 'course10',
-		price: 'Karan Ganesan',
-		twitter: 'karanganesan',
+		price: '£3',
+		topic: 'math',
 		website: 'linkedin.com/in/karanganesan',
 		github: 'github.com/karanganesan/Lost_in_Metaverse'
 	}
