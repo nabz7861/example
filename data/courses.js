a0a3870079da708ce540bc8b1204d65ce620af67
Nabeel Hussain <nab_h@hotmail.co.uk>
2020-02-02 18:19:53 +0000

    feb

--- data/courses.js
+++ data/courses.js
@@ -2,7 +2,7 @@ var courses = [
 	{
 		slug: 'course1',
 		name: 'course1',
-		price: '£3',
+		price: '100',
 		topic: 'math',
 		rating: '5',
 		locate: 'hendon'
@@ -10,74 +10,74 @@ var courses = [
 	{
 		slug: 'course2',
 		name: 'course2',
-		price: '£3',
+		price: '80',
 		topic: 'math',
 		rating: '5',
-        locate: 'hendon'
+        locate: 'colindale'
 	},
 	{
 		slug: 'course3',
 		name: 'course3',
-		price: '£3',
+		price: '£33',
 		topic: 'math',
 		rating: '5',
-		locate: 'hendon'
+		locate: 'brent cross'
 	},
 	{
 		slug: 'course4',
 		name: 'course4',
-		price: '£3',
+		price: '£43',
 		topic: 'math',
 		rating: '5',
-		locate: 'hendon'
+		locate: 'golders green'
 	},
 	{
 		slug: 'course5',
 		name: 'course5',
-		price: '£3',
-		topic: 'math',
+		price: '£32',
+		topic: 'english',
 		rating: '5',
 		locate: 'hendon'
 	},
 	{
 		slug: 'course6',
 		name: 'course6',
-		price: '£3',
-		topic: 'math',
+		price: '£33',
+		topic: 'english',
 		rating: '5',
-		locate: 'hendon'
+		locate: 'colindale'
 	},
 	{
 		slug: 'course7',
 		name: 'course7',
-		price: '£3',
-		topic: 'math',
+		price: '£34',
+		topic: 'english',
 		rating: '5',
-		locate: 'hendon'
+		locate: 'brent cross'
 	},
 	{
 		slug: 'course8',
 		name: 'course8',
-		price: '£3',
-		topic: 'math',
+		price: '£35',
+		topic: 'english',
 		rating: '5',
-		locate: 'hendon'
+		locate: 'golders green'
 	},
 	{
 		slug: 'course9',
 		name: 'course9',
-		price: '£3',
-		topic: 'math',
+		price: '£34',
+		topic: 'piano',
 		rating: '5',
-		locate: 'hendon'
+		locate: 'golders green'
 	},
 	
 	{
 		slug: 'course10',
 		name: 'course10',
-		price: '£3',
-		topic: 'math',
-		rating: '5',
-		locate: 'hendon'
+		price: '£34',
+		topic: 'piano',
+		rating: '54',
+		locate: 'golders green'
 	}
 ];
